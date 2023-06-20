cadf7c16907fc08cb88f10d4e4d4cfecfe675036
Bhavya Rani <129007686+br-23@users.noreply.github.com>
2023-06-21 04:55:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var random1= Math.floor(Math.random()*6+1);
+document.querySelector(".img1").setAttribute("src","./images/dice"+random1+".png");
+
+var random2= Math.floor(Math.random()*6+1);
+document.querySelector(".img2").setAttribute("src","./images/dice"+random2+".png");
+
+if(random1>random2){
+    document.querySelector("h1").innerHTML= "🎉 Player 1 wins";
+}
+else if(random1<random2){
+    document.querySelector("h1").innerHTML= "Player 2 wins 🎉";
+}
+else{
+    document.querySelector("h1").innerHTML= "💀Draw";
+}
+
